5a6462d659ab5d39e65e458f407b06f105161a73
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-05-21 12:02:08 +0800

    update file

--- WikiWeb2M/download_datasets.sh
+++ WikiWeb2M/download_datasets.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-# Create directories
-mkdir "train"
-mkdir "valid"
-mkdir "test"
-
-# Download training set
+## Download datasets
+echo "Downloading datasets..."
 for i in {0..4}; do
-    wget -P "train" "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2m-train.tfrecord.gz-0000${i}-of-00005"
+    echo "Downloading wikiweb2m-train.tfrecord.gz-0000${i}-of-00005"
+    wget "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2m-train.tfrecord.gz-0000${i}-of-00005"
 done
+echo "Downloading wikiweb2m-val.tfrecord.gz"
+wget "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2m-val.tfrecord.gz"
+echo "Downloading wikiweb2m-test.tfrecord.gz"
+wget "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2m-test.tfrecord.gz"
 
-# Download validation set
-wget -P "valid" "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2m-val.tfrecord.gz"
-
-# Download test set
-wget -P "test" "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2m-test.tfrecord.gz"
-
-# Extract compressed files
+## Extract compressed files
+echo "Extracting compressed files..."
+echo "Combining wikiweb2m-train.tfrecord.gz parts into a single file"
 cat wikiweb2m-train.tfrecord.gz-000* > wikiweb2m-train.tfrecord.gz
 rm wikiweb2m-train.tfrecord.gz-000*
+echo "Decompressing wikiweb2m-train.tfrecord.gz"
 gzip -d "wikiweb2m-train.tfrecord.gz"
+echo "Decompressing wikiweb2m-val.tfrecord.gz"
 gzip -d "wikiweb2m-val.tfrecord.gz"
+echo "Decompressing wikiweb2m-test.tfrecord.gz"
 gzip -d "wikiweb2m-test.tfrecord.gz"
 
-# Create index files
+## Create index files
 # pip3 install tfrecord
+echo "Creating index files..."
+echo "Creating index for wikiweb2m-train.tfrecord"
 python3 -m tfrecord.tools.tfrecord2idx wikiweb2m-train.tfrecord wikiweb2m-train.tfidnex
+echo "Creating index for wikiweb2m-val.tfrecord"
 python3 -m tfrecord.tools.tfrecord2idx wikiweb2m-val.tfrecord wikiweb2m-val.tfidnex
+echo "Creating index for wikiweb2m-test.tfrecord"
 python3 -m tfrecord.tools.tfrecord2idx wikiweb2m-test.tfrecord wikiweb2m-test.tfidnex
+echo "Done!"
