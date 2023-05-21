9cfa81b356da17916003b6c6505ae353e34221f7
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-05-21 11:42:13 +0800

    update file

--- WikiWeb2M/download_datasets.sh
+++ WikiWeb2M/download_datasets.sh
@@ -15,3 +15,16 @@ wget -P "valid" "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2
 
 # Download test set
 wget -P "test" "https://storage.googleapis.com/gresearch/wit/wikiweb2m/wikiweb2m-test.tfrecord.gz"
+
+# Extract compressed files
+cat wikiweb2m-train.tfrecord.gz-000* > wikiweb2m-train.tfrecord.gz
+rm wikiweb2m-train.tfrecord.gz-000*
+gzip -d "wikiweb2m-train.tfrecord.gz"
+gzip -d "wikiweb2m-val.tfrecord.gz"
+gzip -d "wikiweb2m-test.tfrecord.gz"
+
+# Create index files
+# pip3 install tfrecord
+python3 -m tfrecord.tools.tfrecord2idx wikiweb2m-train.tfrecord wikiweb2m-train.tfidnex
+python3 -m tfrecord.tools.tfrecord2idx wikiweb2m-val.tfrecord wikiweb2m-val.tfidnex
+python3 -m tfrecord.tools.tfrecord2idx wikiweb2m-test.tfrecord wikiweb2m-test.tfidnex
